1350b7b71830e420b3e77003f226d9d015cca776
Jonahuto <jonathan66ruliano@gmail.com>
2023-09-24 11:44:05 +0700

    Menambahkan file latihan1.php

---
+++ latihan1.php
@@ -0,0 +1,10 @@
+<?php
+class latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama Saya Jonathan Ruliano
+             Saya tinggal di BPI.";
+    }
+}
